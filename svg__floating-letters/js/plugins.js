770c4ae4d3c0ba8160ea398bd198f69aa2e0cd1d
quant-front <62645941+quant-front@users.noreply.github.com>
2020-07-25 12:06:13 +0300

    Add files via upload

---
+++ svg__floating-letters/js/plugins.js
@@ -0,0 +1,38 @@
+anime({
+     targets:'.main span:nth-child(1)',
+     duration:12000,
+     translateY: [-75+'%',25+'%'],
+     easing:'linear',
+     rotate:360,
+     loop:true,
+})
+anime({
+     targets:'.main span:nth-child(2)',
+     duration:12000,
+     translateY: [-90+'%',25+'%'],
+     easing:'linear',
+     rotate:360,
+     loop:true,
+     delay: 2000,
+})
+anime({
+     targets:'.main span:nth-child(3)',
+     duration:12000,
+     translateY: [-75+'%',25+'%'],
+     easing:'linear',
+     rotate:360,
+     loop:true,
+})
+
+
+anime({
+     targets:'.main span:nth-child(4)',
+     duration:12000,
+     translateY: [-95+'%',25+'%'],
+     easing:'linear',
+     rotate:360,
+     loop:true,
+     delay: 2000,
+})
+
+
