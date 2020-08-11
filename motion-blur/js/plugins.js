e3bbc4f5ff100c788b05b65d8549c587ea1642ac
quant-front <62645941+quant-front@users.noreply.github.com>
2020-08-11 11:25:16 +0300

    Add files via upload

---
+++ motion-blur/js/plugins.js
@@ -0,0 +1,12 @@
+     // //
+     // setTimeout(function () {
+     //      anime({
+     //           targets: '.circle',
+     //           left:'800px',
+     //           duration: 700,
+     //           easing: 'easeInQuad',
+     //           direction:'alternate',
+     //           loop: true
+     //      });
+     //
+     // },100);
