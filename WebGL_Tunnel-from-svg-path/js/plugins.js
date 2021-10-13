8d0eeb061fa18808ebfe88b96eb554265f0d79e6
quant-front <62645941+quant-front@users.noreply.github.com>
2021-10-13 16:52:44 +0300

    Add files via upload

---
+++ WebGL_Tunnel-from-svg-path/js/plugins.js
@@ -0,0 +1,76 @@
+import * as THREE from 'three/build/three.module.js';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor(0xECF0F1);
+let doc = document.querySelector('.main');
+doc.appendChild( renderer.domElement );
+
+
+window.addEventListener('resize', function () {
+     let width = window.innerWidth;
+     let height = window.innerHeight;
+     renderer.setSize(width,height);
+     renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+     camera.aspect = width / height;
+     camera.updateProjectionMatrix();
+});
+
+
+
+let controls = new  OrbitControls(camera,renderer.domElement);
+
+
+
+//Array of points
+const points = [
+     [68.5,185.5],
+     [1,262.5],
+     [270.9,281.9],
+     [345.5,212.8],
+     [178,155.7],
+     [240.3,72.3],
+     [153.4,0.6],
+     [52.6,53.3],
+     [68.5,185.5]
+];
+
+//Convert the array of points into vertices
+for (let i = 0; i < points.length; i++) {
+     let x = points[i][0];
+     let y = 0;
+     let z = points[i][1];
+     points[i] = new THREE.Vector3(x, y, z);
+}
+//Create a path from the points
+const path = new THREE.CatmullRomCurve3(points);
+
+//Create the tube geometry from the path
+const geometry = new THREE.TubeGeometry( path, 300, 2, 20, true );
+//Basic material
+const material = new THREE.MeshBasicMaterial( { color: 0xff0000, side: THREE.DoubleSide} );
+//Create a mesh
+const tube = new THREE.Mesh( geometry, material );
+//Add tube into the scene
+scene.add( tube );
+tube.scale.multiplyScalar(0.3);
+tube.position.x = -40;
+tube.position.y = -40;
+tube.rotation.x = Math.PI * -.4;
+
+
+
+camera.position.z = 190;
+
+const animate = function () {
+     requestAnimationFrame( animate );
+
+     renderer.render( scene, camera );
+};
+
+animate();
