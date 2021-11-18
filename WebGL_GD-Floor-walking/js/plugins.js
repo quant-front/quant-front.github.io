08f82b1f6b36f824a12040405a82fc1ca2e099e5
quant-front <62645941+quant-front@users.noreply.github.com>
2021-11-18 14:38:57 +0200

    Add files via upload

--- WebGL_GD-Floor-walking/js/plugins.js
+++ WebGL_GD-Floor-walking/js/plugins.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three/build/three.module.js';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import anime from "animejs";
+import { Raycaster } from "three/src/core/Raycaster";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
@@ -30,71 +31,50 @@ controls.addEventListener('end', () => {
      updateCameraOrbit();
 });
 updateCameraOrbit();
+document.addEventListener('dragend', function () {
+     console.log("drag! end");
+})
+
 
 let light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.setScalar(1);
 scene.add(light, new THREE.AmbientLight(0xffffff, 0.5));
 
 let planeGeometry = new THREE.PlaneGeometry(100, 100, 50, 50);
-let material = new THREE.MeshBasicMaterial({
-     color: 0xffffff,
+// let material = new THREE.MeshBasicMaterial({
+//      color: 0xffffff,
+//      wireframe: true
+// });
+let material = new THREE.MeshNormalMaterial({
+     // color: 0xffffff,
      wireframe: true
 });
 const plane = new THREE.Mesh(planeGeometry, material);
 plane.rotateX(-Math.PI / 2);
 scene.add(plane);
 plane.name = '101'
+plane.userData.ground = true;
+let onj = [];
+onj.push(plane);
 
-let cubes = [];
-let mat, geo;
-for (let i = 0; i < 30; i++) {
-     geo = new THREE.BoxGeometry(
-          Math.random() * 4,
-          Math.random() * 16,
-          Math.random() * 4
-     );
-     mat = new THREE.MeshBasicMaterial({ wireframe: false });
-     switch (i % 3) {
-          case 0:
-               mat.color = new THREE.Color(0xff0000);
-               break;
-          case 1:
-               mat.color = new THREE.Color(0xffff00);
-               break;
-          case 2:
-               mat.color = new THREE.Color(0x0000ff);
-               break;
+document.addEventListener( 'mousemove', onDocumentMouseMovePointsGirl, false );
+
+function onDocumentMouseMovePointsGirl(event) {
+
+     const mouse = new THREE.Vector2();
+     mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+     mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+     const raycaster = new Raycaster();
+     raycaster.setFromCamera( mouse, camera );
+     const intersects = raycaster.intersectObjects( onj  );
+     if(intersects.length > 0) {
+          document.body.style.cursor = "pointer";
+     } else {
+          document.body.style.cursor = "grab";
      }
-     const cube = new THREE.Mesh(geo, mat);
-     // cubes.push(cube);
 }
-cubes.forEach((c) => {
-     c.position.x = Math.random() * 100 - 50;
-     c.position.z = Math.random() * 100 - 50;
-     c.geometry.computeBoundingBox();
-     c.position.y =
-          (c.geometry.boundingBox.max.y - c.geometry.boundingBox.min.y) / 2;
-     scene.add(c);
-});
 
-let spots = [];
-for (let i = 0; i < 30; i++) {
-     geo = new THREE.BoxGeometry(4, 0.4, 4);
-     mat = new THREE.MeshBasicMaterial({ wireframe: false });
-
-     mat.color = new THREE.Color(0xffffff);
-
-     let spot = new THREE.Mesh(geo, mat);
-     spots.push(spot);
-}
-spots.forEach((c) => {
-     c.position.x = Math.random() * 100 - 50;
-     c.position.z = Math.random() * 100 - 50;
-     c.geometry.computeBoundingBox();
-     c.position.y = 0.2;
-     scene.add(c);
-});
-
+// walking
 function updateCameraOrbit() {
      var forward = new THREE.Vector3();
      camera.getWorldDirection(forward);
@@ -116,8 +96,9 @@ window.addEventListener('click', (event) => {
           if (k === 1) {
                const point = found[0].point;
                point.y = camera.position.y;
+
                if (found[0].object.name === '101') {
-                  console.log("true");
+
                }
                else {
                     console.log("false");
@@ -129,10 +110,10 @@ window.addEventListener('click', (event) => {
                     z: found[0].point.z,
                     easing:'easeOutCubic',
                     loop:1,
+                    delay:100,
                     duration:found[0].distance * 70,
                     update:function(anim){
                          updateCameraOrbit();
-                         camera.lookAt( scene.position );
                     },
                     complete:function(anim){
                          updateCameraOrbit();
@@ -149,7 +130,77 @@ window.addEventListener('click', (event) => {
 
 
      }
-})
+});
+
+// mouse cursor
+
+let draggable;
+
+const geometry = new THREE.SphereGeometry( 1, 32, 16 );
+const material1 = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const sphere = new THREE.Mesh( geometry, material1 );
+// scene.add( sphere );
+// sphere.userData.draggable = true;
+// sphere.userData.name = 'SPHERE'
+
+
+const geometry8 = new THREE.RingBufferGeometry(0.45, 0.5, 32).rotateX(
+     -Math.PI / 2
+);
+const material8 = new THREE.MeshBasicMaterial();
+
+var meshcircle = new THREE.Mesh(geometry8, material8);
+scene.add(meshcircle);
+draggable = meshcircle;
+
+
+const raycaster = new THREE.Raycaster(); // create once
+const moveMouse = new THREE.Vector2();
+
+function intersect(pos) {
+     raycaster.setFromCamera(pos, camera);
+     return raycaster.intersectObjects(scene.children);
+}
+
+
+window.addEventListener('mousemove', event => {
+     moveMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+     moveMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+});
+
+function dragObject() {
+     if (draggable != null) {
+          const found = intersect(moveMouse);
+          if (found.length > 0) {
+               for (let i = 0; i < found.length; i++) {
+                    if (!found[i].object.userData.ground)
+                         continue
+                    let target = found[i].point;
+                    draggable.position.x = target.x
+                    draggable.position.z = target.z
+               }
+          }
+     }
+}
+
+// Change visible
+
+const cursorObj = [];
+cursorObj.push(plane);
+function onDocumentMouseMove(event) {
+
+     const mouse = new THREE.Vector2();
+     mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+     mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+     // const raycaster = new Raycaster();
+     raycaster.setFromCamera( mouse, camera );
+     const intersects = raycaster.intersectObjects( cursorObj );
+
+     meshcircle.visible = intersects.length > 0;
+
+}
+
+document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
 
 camera.position.z = 10;
@@ -157,6 +208,7 @@ camera.position.y = 3;
 
 const animate = function () {
      requestAnimationFrame( animate );
+     dragObject();
      updateCameraOrbit();
      renderer.render( scene, camera );
      controls.update();
