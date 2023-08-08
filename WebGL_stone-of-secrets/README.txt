b7d54d1b2daf05be1647a480d6806e1e26704050
light1235 <kh.ultra1234@gmail.com>
2023-08-08 23:27:30 +0300

    no

--- WebGL_stone-of-secrets/README.txt
+++
@@ -1,2 +0,0 @@
-To run the project install chrome dev server
-https://chrome.google.com/webstore/detail/web-server-for-chrome/ofhbbkphhbklhfoeikjpcbhemlocgigb
--- WebGL_stone-of-secrets/assets/shaders/vertex.glsl
+++
@@ -1,7 +0,0 @@
-varying vec3 v_position;
-varying vec2 vUv;
-uniform float time;
-void main() {
- vUv = uv;
- gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-}
